a5c3260c61fc155cb76f93fd802adc23f3549178
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2023-05-29 19:38:26 -0700

    Compression Abstractions (#3)

---
+++ src/BaroquenMelody.Library/Compression/BrotliBitArrayCompressor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.IO.Compression;
+
+namespace BaroquenMelody.Library.Compression;
+
+/// <inheritdoc cref="IBitArrayCompressor"/>
+internal sealed class BrotliBitArrayCompressor : IBitArrayCompressor
+{
+    private readonly CompressionLevel _compressionLevel;
+
+    public BrotliBitArrayCompressor(CompressionLevel compressionLevel) => _compressionLevel = compressionLevel;
+
+    public byte[] Compress(BitArray bits)
+    {
+        var bytes = new byte[(bits.Length - 1) / 8 + 1];
+        bits.CopyTo(bytes, 0);
+
+        using var originalStream = new MemoryStream(bytes);
+        using var compressedStream = new MemoryStream();
+
+        using (var compressionStream = new BrotliStream(compressedStream, _compressionLevel))
+        {
+            originalStream.CopyTo(compressionStream);
+        }
+
+        return compressedStream.ToArray();
+    }
+
+    public BitArray Decompress(byte[] bytes)
+    {
+        using var compressedStream = new MemoryStream(bytes);
+        using var decompressedStream = new MemoryStream();
+
+        using (var decompressionStream = new BrotliStream(compressedStream, CompressionMode.Decompress))
+        {
+            decompressionStream.CopyTo(decompressedStream);
+        }
+
+        return new BitArray(decompressedStream.ToArray());
+    }
+}
---
+++ src/BaroquenMelody.Library/Compression/IBitArrayCompressor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace BaroquenMelody.Library.Compression;
+
+/// <summary>
+///     A compressor for <see cref="BitArray" />s.
+/// </summary>
+public interface IBitArrayCompressor
+{
+    /// <summary>
+    ///     Compresses a <see cref="BitArray" /> into a byte array.
+    /// </summary>
+    /// <param name="bits">The <see cref="BitArray" /> to compress.</param>
+    /// <returns>The compressed <see cref="BitArray" /> as a byte array.</returns>
+    byte[] Compress(BitArray bits);
+
+    /// <summary>
+    ///     Decompresses a byte array into a <see cref="BitArray" />.
+    /// </summary>
+    /// <param name="bytes">The byte array to decompress.</param>
+    /// <returns>The decompressed byte array as a <see cref="BitArray" />.</returns>
+    BitArray Decompress(byte[] bytes);
+}
