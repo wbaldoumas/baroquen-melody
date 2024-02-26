89c025c3160fcfb25795b7949652d817bcda47d6
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2024-02-25 17:41:35 -0800

    Composition evaluation (#47)

---
+++ src/BaroquenMelody.Library/Compositions/Evaluations/Rules/ICompositionRule.cs
@@ -0,0 +1,16 @@
+﻿using BaroquenMelody.Library.Compositions.Domain;
+
+namespace BaroquenMelody.Library.Compositions.Evaluations.Rules;
+
+/// <summary>
+///     A composition rule to be checked against a composition.
+/// </summary>
+internal interface ICompositionRule
+{
+    /// <summary>
+    ///     Evaluates the given composition for any violations of the rule.
+    /// </summary>
+    /// <param name="composition">The composition to be evaluated.</param>
+    /// <returns>The evaluated composition.</returns>
+    Composition EvaluateComposition(Composition composition);
+}
---
+++ tests/BaroquenMelody.Library.Tests/Compositions/Evaluations/CompositionInstructorTests.cs
@@ -0,0 +1,50 @@
+﻿using BaroquenMelody.Library.Compositions.Domain;
+using BaroquenMelody.Library.Compositions.Evaluations;
+using BaroquenMelody.Library.Compositions.Evaluations.Rules;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BaroquenMelody.Library.Tests.Compositions.Evaluations;
+
+[TestFixture]
+internal sealed class CompositionInstructorTests
+{
+    private ICompositionRule _mockCompositionRuleA = null!;
+
+    private ICompositionRule _mockCompositionRuleB = null!;
+
+    private CompositionInstructor _compositionInstructor = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockCompositionRuleA = Substitute.For<ICompositionRule>();
+        _mockCompositionRuleB = Substitute.For<ICompositionRule>();
+
+        _compositionInstructor = new CompositionInstructor(
+        [
+            _mockCompositionRuleA,
+            _mockCompositionRuleB
+        ]);
+    }
+
+    [Test]
+    public void WhenEvaluateCompositionIsInvoked_ThenCompositionIsReturned()
+    {
+        // arrange
+        var composition = new Composition([]);
+
+        _mockCompositionRuleA.EvaluateComposition(composition).Returns(composition);
+        _mockCompositionRuleB.EvaluateComposition(composition).Returns(composition);
+
+        // act
+        var evaluatedComposition = _compositionInstructor.EvaluateComposition(composition);
+
+        // assert
+        evaluatedComposition.Should().Be(composition);
+
+        _mockCompositionRuleA.Received(1).EvaluateComposition(composition);
+        _mockCompositionRuleB.Received(1).EvaluateComposition(composition);
+    }
+}
