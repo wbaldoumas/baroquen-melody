0278403df2e7ab6c95ec815cd0634dffe211dfe0
Will Baldoumas <william.baldoumas@gmail.com>
2024-02-25 17:27:46 -0800

    Composition Evaluation

---
+++ src/BaroquenMelody.Library/Compositions/Evaluations/CompositionInstructor.cs
@@ -0,0 +1,12 @@
+﻿using BaroquenMelody.Library.Compositions.Domain;
+using BaroquenMelody.Library.Compositions.Evaluations.Rules;
+
+namespace BaroquenMelody.Library.Compositions.Evaluations;
+
+internal sealed class CompositionInstructor(IEnumerable<ICompositionRule> compositionRules) : ICompositionInstructor
+{
+    public Composition EvaluateComposition(Composition composition) => compositionRules.Aggregate(
+        composition,
+        (current, compositionRule) => compositionRule.EvaluateComposition(current)
+    );
+}
