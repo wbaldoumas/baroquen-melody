f630bc3695767d4b4c41e1a31c2f77fbbb66c227
Will Baldoumas <william.baldoumas@gmail.com>
2024-07-28 21:11:25 -0700

    Coverage exceptions

--- src/BaroquenMelody.Library/Infrastructure/Exceptions/CouldNotFindStartingNoteForVoiceException.cs
+++ src/BaroquenMelody.Library/Infrastructure/Exceptions/CouldNotFindStartingNoteForVoiceException.cs
@@ -1,5 +1,7 @@
 ﻿using BaroquenMelody.Library.Compositions.Enums;
+using System.Diagnostics.CodeAnalysis;
 
 namespace BaroquenMelody.Library.Infrastructure.Exceptions;
 
+[ExcludeFromCodeCoverage(Justification = "Exception with no logic.")]
 internal sealed class CouldNotFindStartingNoteForVoiceException(Voice voice) : Exception($"Could not find starting note for voice {voice}.");
--- src/BaroquenMelody.Library/Infrastructure/Exceptions/NoValidChordChoicesAvailableException.cs
+++ src/BaroquenMelody.Library/Infrastructure/Exceptions/NoValidChordChoicesAvailableException.cs
@@ -1,3 +1,6 @@
-﻿namespace BaroquenMelody.Library.Infrastructure.Exceptions;
+﻿using System.Diagnostics.CodeAnalysis;
 
+namespace BaroquenMelody.Library.Infrastructure.Exceptions;
+
+[ExcludeFromCodeCoverage(Justification = "Exception with no logic.")]
 internal sealed class NoValidChordChoicesAvailableException() : Exception("No valid chord choices are available.");
