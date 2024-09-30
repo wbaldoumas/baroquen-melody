f9d16a51416c66b05f046eabdcdc5e46691fdc6f
Will Baldoumas <william.baldoumas@gmail.com>
2024-09-29 17:49:27 -0700

    Adjust Locked / Enabled Status in UI

--- tests/BaroquenMelody.Library.Tests/Configuration/Enums/Extensions/ConfigurationStatusExtensionsTests.cs
+++ tests/BaroquenMelody.Library.Tests/Configuration/Enums/Extensions/ConfigurationStatusExtensionsTests.cs
@@ -9,32 +9,14 @@ namespace BaroquenMelody.Library.Tests.Configuration.Enums.Extensions;
 internal sealed class ConfigurationStatusExtensionsTests
 {
     [Test]
-    [TestCase(ConfigurationStatus.Enabled, ConfigurationStatus.Locked)]
-    [TestCase(ConfigurationStatus.Locked, ConfigurationStatus.Disabled)]
-    [TestCase(ConfigurationStatus.Disabled, ConfigurationStatus.Enabled)]
-    public void Cycle_returns_the_next_value(ConfigurationStatus status, ConfigurationStatus expectedNextStatus)
-    {
-        // act
-        var nextStatus = status.Cycle();
-
-        // assert
-        nextStatus.Should().Be(expectedNextStatus);
-    }
-
-    [Test]
-    public void Cycle_throws_when_the_source_configuration_status_is_not_a_valid_value()
-    {
-        // act
-        var act = () => ((ConfigurationStatus)byte.MaxValue).Cycle();
-
-        // assert
-        act.Should().Throw<ArgumentOutOfRangeException>();
-    }
-
-    [Test]
+    [TestCase(ConfigurationStatus.None, false)]
     [TestCase(ConfigurationStatus.Enabled, true)]
-    [TestCase(ConfigurationStatus.Locked, true)]
+    [TestCase(ConfigurationStatus.Locked, false)]
     [TestCase(ConfigurationStatus.Disabled, false)]
+    [TestCase(ConfigurationStatus.EnabledAndLocked, true)]
+    [TestCase(ConfigurationStatus.DisabledAndLocked, false)]
+    [TestCase(ConfigurationStatus.Enabled | ConfigurationStatus.Locked, true)]
+    [TestCase(ConfigurationStatus.Disabled | ConfigurationStatus.Locked, false)]
     public void IsEnabled_returns_whether_the_configuration_is_enabled(ConfigurationStatus status, bool expectedIsEnabled)
     {
         // act
@@ -45,9 +27,14 @@ internal sealed class ConfigurationStatusExtensionsTests
     }
 
     [Test]
+    [TestCase(ConfigurationStatus.None, true)]
     [TestCase(ConfigurationStatus.Enabled, false)]
     [TestCase(ConfigurationStatus.Locked, true)]
     [TestCase(ConfigurationStatus.Disabled, true)]
+    [TestCase(ConfigurationStatus.EnabledAndLocked, true)]
+    [TestCase(ConfigurationStatus.DisabledAndLocked, true)]
+    [TestCase(ConfigurationStatus.Enabled | ConfigurationStatus.Locked, true)]
+    [TestCase(ConfigurationStatus.Disabled | ConfigurationStatus.Locked, true)]
     public void IsFrozen_returns_whether_the_configuration_is_frozen(ConfigurationStatus status, bool expectedIsFrozen)
     {
         // act
