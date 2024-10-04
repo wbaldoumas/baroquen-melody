cc91a9b261294be6c37bb247d29ed87b86cb71ac
Will Baldoumas <william.baldoumas@gmail.com>
2024-10-04 15:32:05 -0700

    Cleanup

--- src/BaroquenMelody.Library/Configurations/Serialization/JsonConverters/SevenBitNumberJsonConverter.cs
+++ src/BaroquenMelody.Library/Configurations/Serialization/JsonConverters/SevenBitNumberJsonConverter.cs
@@ -8,17 +8,7 @@ public sealed class SevenBitNumberJsonConverter : JsonConverter<SevenBitNumber>
 {
     private static readonly SevenBitNumber DefaultSevenBitNumber = new(50);
 
-    public override SevenBitNumber Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        var value = reader.GetString();
-
-        if (SevenBitNumber.TryParse(value, out var sevenBitNumber))
-        {
-            return sevenBitNumber;
-        }
-
-        return DefaultSevenBitNumber;
-    }
+    public override SevenBitNumber Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => SevenBitNumber.TryParse(reader.GetString(), out var sevenBitNumber) ? sevenBitNumber : DefaultSevenBitNumber;
 
     public override void Write(Utf8JsonWriter writer, SevenBitNumber value, JsonSerializerOptions options) => writer.WriteStringValue(value.ToString());
 }
