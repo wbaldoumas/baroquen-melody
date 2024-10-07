7427d79fdfd8627d7751a538fbce4f9ef175a5e0
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2024-10-06 17:46:10 -0700

    Break Up Instrumentation Card (#185)

---
+++ src/BaroquenMelody.Library/Store/Actions/UpdateInstrumentTonalRange.cs
@@ -0,0 +1,6 @@
+﻿using BaroquenMelody.Library.Enums;
+using Melanchall.DryWetMidi.MusicTheory;
+
+namespace BaroquenMelody.Library.Store.Actions;
+
+public sealed record UpdateInstrumentTonalRange(Instrument Instrument, Note LowestPitchNote, Note HighestPitchNote);
---
+++ src/BaroquenMelody.Library/Store/Actions/UpdateMidiInstrument.cs
@@ -0,0 +1,6 @@
+﻿using BaroquenMelody.Library.Enums;
+using Melanchall.DryWetMidi.Standards;
+
+namespace BaroquenMelody.Library.Store.Actions;
+
+public sealed record UpdateMidiInstrument(Instrument Instrument, GeneralMidi2Program MidiInstrument);
