4d3584aed19f8e872c14b5fd2ec32e269a736f60
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2024-10-09 17:34:53 -0700

    Add About (#194)

---
+++ src/BaroquenMelody.App/Infrastructure/Application/ApplicationInfo.cs
@@ -0,0 +1,25 @@
+﻿using BaroquenMelody.Infrastructure.Application;
+using System.Globalization;
+
+namespace BaroquenMelody.App.Infrastructure.Application;
+
+internal sealed class ApplicationInfo(IAppInfo appInfo) : IApplicationInfo
+{
+    public string Version => appInfo.Version.ToString(3);
+
+    public string Build => appInfo.BuildString;
+
+    public string Commit => ThisAssembly.Git.Commit;
+
+    public string CommitDate => DateTime.Parse(ThisAssembly.Git.CommitDate, CultureInfo.InvariantCulture).ToString("dd-MMM-yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+
+    public string Sha => ThisAssembly.Git.Sha;
+
+    public string RepositoryUrl => "https://github.com/wbaldoumas/baroquen-melody";
+
+    public string SupportUrl => "https://buymeacoffee.com/baroquenmelody";
+
+    public string ContributeUrl => "https://github.com/wbaldoumas/baroquen-melody/blob/main/CONTRIBUTING.md";
+
+    public string LicenseUrl => "https://github.com/wbaldoumas/baroquen-melody/blob/main/LICENSE";
+}
