3a9d8670f9969004273bc95b249065854c4a1c39
Will Baldoumas <william.baldoumas@gmail.com>
2025-01-11 17:58:35 -0800

    Cleanup

---
+++ tests/BaroquenMelody.Library.Tests/Store/Reducers/SavedCompositionConfigurationReducersTests.cs
@@ -0,0 +1,25 @@
+﻿using BaroquenMelody.Library.Store.Actions;
+using BaroquenMelody.Library.Store.Reducers;
+using BaroquenMelody.Library.Store.State;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BaroquenMelody.Library.Tests.Store.Reducers;
+
+[TestFixture]
+internal sealed class SavedCompositionConfigurationReducersTests
+{
+    [Test]
+    public void ReduceUpdateLastLoadedConfigurationName()
+    {
+        // arrange
+        var state = new SavedCompositionConfigurationState("LastLoadedConfigurationName");
+        var action = new UpdateLastLoadedConfigurationName("NewLastLoadedConfigurationName");
+
+        // act
+        var newState = SavedCompositionConfigurationReducers.ReduceUpdateLastLoadedConfigurationName(state, action);
+
+        // assert
+        newState.LastLoadedConfigurationName.Should().Be("NewLastLoadedConfigurationName");
+    }
+}
