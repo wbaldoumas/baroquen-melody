8d66df6770b608e16fce2bddccc361a91c38329c
Will Baldoumas <william.baldoumas@gmail.com>
2025-01-11 18:11:12 -0800

    Coverage

--- tests/BaroquenMelody.Library.Tests/Store/Reducers/SavedCompositionConfigurationReducersTests.cs
+++ tests/BaroquenMelody.Library.Tests/Store/Reducers/SavedCompositionConfigurationReducersTests.cs
@@ -10,7 +10,21 @@ namespace BaroquenMelody.Library.Tests.Store.Reducers;
 internal sealed class SavedCompositionConfigurationReducersTests
 {
     [Test]
-    public void ReduceUpdateLastLoadedConfigurationName()
+    public void ReduceUpdateLastLoadedConfigurationName_with_empty_state_updates_name()
+    {
+        // arrange
+        var state = new SavedCompositionConfigurationState();
+        var action = new UpdateLastLoadedConfigurationName("NewLastLoadedConfigurationName");
+
+        // act
+        var newState = SavedCompositionConfigurationReducers.ReduceUpdateLastLoadedConfigurationName(state, action);
+
+        // assert
+        newState.LastLoadedConfigurationName.Should().Be("NewLastLoadedConfigurationName");
+    }
+
+    [Test]
+    public void ReduceUpdateLastLoadedConfigurationName_with_existing_state_updates_name()
     {
         // arrange
         var state = new SavedCompositionConfigurationState("LastLoadedConfigurationName");
